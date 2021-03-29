7162f2461795c6ab20df5d8cf31f49366782b214
Bùi Đức Hiếu <42628314+samtiendan@users.noreply.github.com>
2021-03-29 19:09:31 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-let mycaffe = [ 
-    {
-        name:" Caffe cs1",
-        city:"Ha Dong"
-    },{
-        name:" Caffe cs2",
-        city:"Ha Tay"
-    },{
-        name:" Caffe cs3",
-        city:"Ha Nam"
-    },{
-        name:" Caffe cs4",
-        city:"Ha Bac"
-    },{
-        name:" Caffe cs5",
-        city:"Ha Noi"
-    },
-]
-localStorage.setItem('alo', JSON.stringify(mycaffe));
+let app_ID = '7674da634845e7c2d8c53ab0a48b8e29'
+let DEFAULT_VALUE = '--';
+let searchInput = document.querySelector('#search-input');
+let cityName = document.querySelector('.city-name');
+let weatherState = document.querySelector('.weather-state');
+let weatherIcon = document.querySelector('.weather-icon');
+let temperature = document.querySelector('.temperature');
 
-let concua = JSON.parse(localStorage.getItem(mycaffe));
-
-console.log(concua);
-
+searchInput.addEventListener('change', (e) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${app_ID}&units=metric`)
+    .then(async res => {
+        let data = await res.json();
+        console.log('[Search Input]', data);
+        cityName.innerHTML = data.name || DEFAULT_VALUE;
+        weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
+        weatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+        temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
+    });
+});
